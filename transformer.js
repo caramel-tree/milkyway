617f3820401bce8f8bf91991417e099bbf4e40f1
preqelside <preqel@naver.com>
2021-08-23 12:48:42 +0900

    Create transformer.js

---
+++ transformer.js
@@ -0,0 +1,27 @@
+if ($(window).width() > 799) $(function(){
+    'use strict';
+
+    var $page = $('#main'),
+        options = {
+            debug: false,
+            prefetch: true,
+            cacheLength: 15,
+            forms: 'form',
+            onStart: {
+                duration: 250,
+                render: function ($container) {
+                    $container.addClass('is-exiting');
+                    smoothState.restartCSSAnimations();
+                }
+            },
+            onReady: {
+                duration: 0,
+                render: function ($container, $newContent) {
+                    $container.removeClass('is-exiting');
+                    $container.html($newContent);
+                    loadPage(smoothState.href);
+            }
+        }
+    },
+    smoothState = $page.smoothState(options).data('smoothState');
+});
